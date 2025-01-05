3e87d7d7f4b732f1327b42058cc816a3abdafca9
Chamil Fonseka <fonseka.chamil@gmail.com>
2025-01-05 20:23:34 +0530

    customized tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: [
@@ -8,6 +9,15 @@ export default {
   ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: "1.5rem",
+        screens: { sm: "100%", md: "100%", lg: "100%", xl: "1280px" },
+      },
+      fontFamily: {
+        sans: ["var(--font-geist-sans)", ...fontFamily.sans],
+        mono: ["var(--font-geist-mono)", ...fontFamily.mono],
+      }
     },
   },
   plugins: [],
