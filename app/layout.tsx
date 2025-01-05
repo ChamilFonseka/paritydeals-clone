bc5a14229f34832be9544d0583de5032b707efc8
Chamil Fonseka <fonseka.chamil@gmail.com>
2025-01-05 21:24:53 +0530

    customized shadcn accent colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} 
-        antialiased font-sans`}
+        antialiased font-sans bg-background`}
       >
         {children}
       </body>
