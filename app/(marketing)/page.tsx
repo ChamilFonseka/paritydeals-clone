77791c0813b03df41619bad388e6b21b89c06e25
Chamil Fonseka <fonseka.chamil@gmail.com>
2025-01-09 21:37:57 +0530

    implemented pricing section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,10 @@
 import { Button } from "@/components/ui/button";
 import { SignUpButton } from "@clerk/nextjs";
-import { ArrowRightIcon } from "lucide-react";
+import { ArrowRightIcon, CheckIcon } from "lucide-react";
 import Image from "next/image";
+import { subscriptionTiresInOrder } from "../data/subscriptionTires";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { cn, formatCompactNumber } from "@/lib/utils";
 
 export default function HomePage() {
   return (
@@ -38,6 +41,71 @@ export default function HomePage() {
           </div>
         </div>
       </section>
+      <section id="pricing" className="px-8 py-16 bg-accent/5 scroll-mt-8">
+        <h2 className=" text-4xl text-center text-balance font-semibold mb-8">
+          Price your product on each market willingness to pay
+        </h2>
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4 max-w-screen-xl mx-auto">
+          {
+            subscriptionTiresInOrder.map(tier => (
+              <PricingCard key={tier.name} {...tier} />
+            ))}
+        </div>
+      </section>
     </>
+  );
+}
+
+function PricingCard({
+  name,
+  priceInCents,
+  maxNumberOfProducts,
+  maxNumberOfVisits,
+  canAccessAnalytics,
+  canCustomizeBanner,
+  canReomveBranding,
+}: typeof subscriptionTiresInOrder[number]) {
+  const mostPopular = name === "Standard";
+  return (
+    <Card>
+      <CardHeader>
+        <div className="text-accent font-semibold mb-8">
+          {name}
+        </div>
+        <CardTitle className="text-xl font-bold">{`$${priceInCents / 100}/mo`}</CardTitle>
+        <CardDescription>
+          {`${formatCompactNumber(maxNumberOfVisits)} pricing page visits /mo`}
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <SignUpButton>
+          <Button className="text-lg w-full rounded-lg" variant={mostPopular ? "accent" : "default"}>Get Started</Button>
+        </SignUpButton>
+      </CardContent>
+      <CardFooter className="flex flex-col gap-4 items-start">
+        <Feature className="font-bold">
+          {`
+            ${maxNumberOfProducts} 
+            ${maxNumberOfProducts === 1 ? 'product' : 'products'} 
+          `}
+        </Feature>
+        <Feature>
+          PPP Discounts
+        </Feature>
+        {canAccessAnalytics && <Feature>Advance Analytics</Feature>}
+        {canReomveBranding && <Feature>Remove Branding</Feature>}
+        {canCustomizeBanner && <Feature>Banner Customization</Feature>}
+      </CardFooter>
+    </Card>
+  );
+}
+
+
+function Feature({ children, className }: { children: React.ReactNode, className?: string; }) {
+  return (
+    <div className={cn("flex items-center gap-2", className)}>
+      <CheckIcon className="size-4 stroke-accent bg-accent/25 rounded-full p-0.5" />
+      <span>{children}</span>
+    </div>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+const formatter = new Intl.NumberFormat(undefined, { notation: "compact" });
+export function formatCompactNumber(number: number) {
+  return formatter.format(number);
 }
